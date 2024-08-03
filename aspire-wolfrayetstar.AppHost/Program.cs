e080c948cc4ae9d21f03965a7a49a2f411a24678
Capella87 <capella87@outlook.com>
2024-08-03 20:30:20 +0900

    Move YouTubeSummariser to a component

--- aspire-wolfrayetstar.AppHost/Program.cs
+++ aspire-wolfrayetstar.AppHost/Program.cs
@@ -2,7 +2,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var cache = builder.AddRedis("cache");
 
-var apiApp = builder.AddProject<Projects.WolfRayetStar_Backend>("wolfrayetstar-backend");
+var apiApp = builder.AddProject<Projects.WolfRayetStar_Backend>("wolfrayetstar-backend").WithExternalHttpEndpoints();
 
 
 builder.AddProject<Projects.WolfRayetStar_Front>("wolfrayetstar-front")
--- frontend/Services/ApiAppClientService.cs
+++ frontend/Services/ApiAppClientService.cs
@@ -1,5 +1,3 @@
-using System.Runtime.CompilerServices;
-
 namespace WolfRayetStar.Front.Services;
 
 public interface IApiAppClientService
