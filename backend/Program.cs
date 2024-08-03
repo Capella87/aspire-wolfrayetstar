3a3f22b3a4242f3bec7c847d035639e00969ef5e
Capella87 <capella87@outlook.com>
2024-08-03 17:39:35 +0900

    feat(backend): Implement Summarisation and weatherforecast services

--- backend/Program.cs
+++ backend/Program.cs
@@ -6,14 +6,28 @@ using Azure;
 using Azure.AI.OpenAI;
 
 using Microsoft.AspNetCore.Mvc;
+using WolfRayetStar.Backend.Services;
 
 using OpenAI.Chat;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
 
 // Add services to the container.
+builder.Services.AddHttpClient<IYouTubeVideo, YouTubeVideo>();
+builder.Services.AddScoped<AzureOpenAIClient>(s =>
+{
+    var config = s.GetRequiredService<IConfiguration>();
+    var endpoint = new Uri(config["OpenAI:Endpoint"]);
+    var credentialKey = new AzureKeyCredential(config["OpenAI:Key"]);
+    var azClient = new AzureOpenAIClient(endpoint, credentialKey);
+
+    return azClient;
+});
+
+builder.Services.AddScoped<YouTubeSummariserService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -37,6 +51,39 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.MapGet("/", () => "Hello Wolf-Rayet Star!");
+app.MapGet("/", () => Results.Redirect("/swagger")).ExcludeFromDescription();
+
+var weatherSummaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecastEntries = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-30, 55),
+            weatherSummaries[Random.Shared.Next(weatherSummaries.Length)]
+        ))
+    .ToArray();
+    return forecastEntries;
+})
+    .WithName("GetWeatherForecast")
+    .WithOpenApi();
+
+app.MapPost("/summarise", async ([FromBody] SummaryRequest req, YouTubeSummariserService service) =>
+{
+    var summary = await service.SummariseAsync(req);
+    return summary;
+})
+    .WithName("GetSummary")
+    .WithOpenApi();
 
 app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / .5556);
+}
---
+++ backend/YouTubeSummariserService.cs
@@ -0,0 +1,47 @@
+using Aliencube.YouTubeSubtitlesExtractor.Abstractions;
+using Aliencube.YouTubeSubtitlesExtractor.Models;
+using Azure.AI.OpenAI;
+using Microsoft.AspNetCore.Mvc;
+using OpenAI.Chat;
+
+namespace WolfRayetStar.Backend.Services;
+
+public record SummaryRequest(string Url, string VideoLanguageCode, string? SummaryLanguageCode);
+
+public class YouTubeSummariserService
+{
+    private readonly IYouTubeVideo _ytVideo;
+    private readonly AzureOpenAIClient _openaiClient;
+    private readonly IConfiguration _config;
+
+    public YouTubeSummariserService(IYouTubeVideo ytVideo, AzureOpenAIClient openaiClient, IConfiguration config)
+    {
+        _ytVideo = ytVideo ?? throw new ArgumentNullException(nameof(ytVideo));
+        _openaiClient = openaiClient ?? throw new ArgumentNullException(nameof(openaiClient));
+        _config = config ?? throw new ArgumentNullException(nameof(config));
+    }
+
+    public async Task<string> SummariseAsync(SummaryRequest rqst)
+    {
+        Subtitle subtitle = await this._ytVideo.ExtractSubtitleAsync(rqst.Url, rqst.VideoLanguageCode).ConfigureAwait(false);
+        string caption = subtitle.Content
+            .Select(p => p.Text)
+            .Aggregate((a, b) => $"{a}\n{b}");
+        var chat = this._openaiClient.GetChatClient(this._config["OpenAI:DeploymentName"]);
+        var messages = new List<ChatMessage>()
+        {
+            new SystemChatMessage(this._config["Prompt:System"]),
+            new SystemChatMessage($"Here's the transcript. Summarise it in 5 bullet point items in the given language code of \"{rqst.SummaryLanguageCode}\"."),
+            new UserChatMessage(caption),
+        };
+        var options = new ChatCompletionOptions()
+        {
+            MaxTokens = int.TryParse(this._config["Prompt:MaxTokens"], out var maxTokens) ? maxTokens : 3000,
+            Temperature = float.TryParse(this._config["Prompt:Temperature"], out var temperature) ? temperature : .7f
+        };
+
+        var response = await chat.CompleteChatAsync(messages, options).ConfigureAwait(false);
+        var summary = response.Value.Content[0].Text;
+        return summary;
+    }
+}
