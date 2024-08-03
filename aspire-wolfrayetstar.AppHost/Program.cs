6ba9e995ffc50802f16c5c2a9bf58d8d9b017f48
Capella87 <capella87@outlook.com>
2024-08-03 18:40:02 +0900

    Add Redis Cache

--- aspire-wolfrayetstar.AppHost/Program.cs
+++ aspire-wolfrayetstar.AppHost/Program.cs
@@ -1,10 +1,13 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var cache = builder.AddRedis("cache");
+
 var apiApp = builder.AddProject<Projects.WolfRayetStar_Backend>("wolfrayetstar-backend");
 
 
 builder.AddProject<Projects.WolfRayetStar_Front>("wolfrayetstar-front")
     .WithExternalHttpEndpoints()
+    .WithReference(cache)
     .WithReference(apiApp);
 
 builder.Build().Run();
